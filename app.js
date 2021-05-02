41e09dab20e16d3052d1d18531e0a85c83add369
Ben Chou <ben7152000@gmail.com>
2021-05-02 13:49:51 +0800

    test

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const favicon = require('express-favicon')
 const userPassport = require('./config/passport')
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8081
 
 app.use(cors())
 
